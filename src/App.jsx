0fcdea119d5f1e9ea994fb17ded098f4d35ef400
Arirzwn <aririzwan0@gmail.com>
2024-07-26 08:26:05 +0700

    Perbaikan: Memastikan fungsi handleClear menghapus semua item todo dan tugas yang telah selesai
    - Memperbarui fungsi handleClear untuk mengosongkan array itemTodo dan complete
    - Menambahkan prop onClear ke komponen ButtonClear
    - Memperbaiki atribut onClick di komponen ButtonClear

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { findReturnStatement } from "eslint-plugin-react/lib/util/ast.js";
 
 const todoData = [
   { id: 1, activity: "Membaca", completed: false },
@@ -29,12 +30,28 @@ function TodoList() {
     setItemTodo(itemTodo.filter((item) => item.id !== id));
   }
 
+  function handleDelete(id) {
+    const updateTodo = itemTodo.filter((item) => item.id !== id);
+
+    setItemTodo(updateTodo);
+  }
+
+  function handleClear() {
+    setItemTodo([]);
+    setComplete([]);
+  }
+
   return (
     <>
       <Header />
       <AddTodo onAddTask={handleAddTask} />
-      <Todo itemTodo={itemTodo} onToggle={handleToggleItem} />
+      <Todo
+        itemTodo={itemTodo}
+        onToggle={handleToggleItem}
+        onDelete={handleDelete}
+      />
       <Complete completedItems={complete} />
+      <ButtonClear onClear={handleClear} />
     </>
   );
 }
@@ -94,14 +111,19 @@ function AddTodo({ onAddTask }) {
   );
 }
 
-function Todo({ itemTodo, onToggle }) {
+function Todo({ itemTodo, onToggle, onDelete }) {
   return (
     <>
       <div className="ml-[35px]">
-        <h3 className=" mt-4 font-semibold ">To Do</h3>
+        <h3 className="mt-4 font-semibold">To Do</h3>
         <ul>
           {itemTodo.map((item) => (
-            <Item item={item} key={item.id} onToggle={onToggle} />
+            <Item
+              item={item}
+              key={item.id}
+              onToggle={onToggle}
+              onDelete={onDelete}
+            />
           ))}
         </ul>
       </div>
@@ -109,10 +131,13 @@ function Todo({ itemTodo, onToggle }) {
   );
 }
 
-function Item({ item, onToggle }) {
+function Item({ item, onToggle, onDelete }) {
   return (
-    <li key={item.id} className="flex justify-between mr-[35px] mb-1">
-      <div className="">
+    <li
+      key={item.id}
+      className="text-sm flex  items-center justify-between mr-[35px] "
+    >
+      <div className="flex items-center">
         <input
           type="checkbox"
           value={item.completed}
@@ -120,9 +145,15 @@ function Item({ item, onToggle }) {
           onChange={() => onToggle(item.id)}
           className="mr-2"
         />
-        {item.activity}
+        <span>{item.activity}</span>
       </div>
-      <button className="text-sm">❌</button>
+      <button
+        className="text-3xl text-red-800"
+        onClick={() => onDelete(item.id)}
+      >
+        {" "}
+        &times;
+      </button>
     </li>
   );
 }
@@ -135,10 +166,36 @@ function Complete({ completedItems }) {
         <ul>
           {completedItems.map((item) => (
             <li key={item.id} className="flex justify-between mr-[35px] mb-1">
-              <div>{item.activity}</div>
+              <span className="line-through text-sm">{item.activity}</span>
             </li>
           ))}
         </ul>
+      </div>
+    </>
+  );
+}
+
+function ButtonClear({ onClear }) {
+  return (
+    <>
+      <div className="text-center my-2">
+        <button
+          className="bg-blue-300 p-1.5 text-sm rounded-full font-semibold "
+          onClick={onClear}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
       </div>
     </>
   );
